1fc1a079c72cb1adcd8ca1cc4e8f7fb9237b4507
Enes <9263360+EnessenE@users.noreply.github.com>
2024-06-29 10:50:32 +0200

    Update get_stop_times_from_stop.sql

--- Functions/get_stop_times_from_stop.sql
+++ Functions/get_stop_times_from_stop.sql
@@ -23,7 +23,7 @@ CREATE OR REPLACE FUNCTION public.get_stop_times_from_stop(target uuid, target_s
     LANGUAGE 'sql'
     COST 100 VOLATILE PARALLEL UNSAFE ROWS 1000
     AS $BODY$
-    SELECT
+SELECT
         trips.internal_id,
         stop_times.arrival_time,
         stop_times.departure_time,
@@ -43,25 +43,28 @@ CREATE OR REPLACE FUNCTION public.get_stop_times_from_stop(target uuid, target_s
         routes.text_color,
         stops.stop_type
     FROM
-        stop_times
-        INNER JOIN trips ON stop_times.trip_id = trips.id
-            AND stop_times.data_origin = trips.data_origin
-        INNER JOIN stops ON stop_times.stop_id = stops.id AND stop_times.data_origin = stops.data_origin and stops.stop_type = 1
-		INNER JOIN related_stops ON related_stops.related_stop = stops.internal_id
-        INNER JOIN routes ON trips.route_id = routes.id
+        trips
+    INNER JOIN routes ON trips.route_id = routes.id
             AND trips.data_origin = routes.data_origin
-        INNER JOIN agencies ON routes.agency_id = agencies.id
-            AND routes.data_origin = agencies.data_origin
-        INNER JOIN calendar_dates ON trips.service_id = calendar_dates.service_id
-			AND calendar_dates.data_origin = trips.data_origin
+	INNER JOIN stop_times ON stop_times.trip_id = trips.id
+		AND stop_times.data_origin = trips.data_origin
+	INNER JOIN stops ON stop_times.stop_id = stops.id AND stop_times.data_origin = stops.data_origin 
+	INNER JOIN related_stops ON related_stops.related_stop = stops.internal_id
+	INNER JOIN agencies ON routes.agency_id = agencies.id
+		AND routes.data_origin = agencies.data_origin
+	INNER JOIN calendar_dates ON trips.service_id = calendar_dates.service_id
+		AND calendar_dates.data_origin = trips.data_origin AND ((calendar_dates.date::date >=from_date::date)
+                )
     WHERE
         -- parent_station / station filter
 (primary_stop = target)
+	AND stop_type = target_stop_type
+
             -- Date filter
-            AND((stop_times.arrival_time >= from_time
-                    AND calendar_dates.date::date = from_date)
-                OR(stop_times.arrival_time <= from_time - INTERVAL '12 hours'
-                    AND calendar_dates.date::date = from_date + INTERVAL '1 day'))
+                    
+            AND((stop_times.arrival_time >= from_time::time
+                    AND calendar_dates.date::date >= from_date::date)
+                OR(calendar_dates.date::date > from_date::date ))
             --Prevent showing the trip if the current stop is the last stop
             AND EXISTS(
                 SELECT
@@ -69,12 +72,16 @@ CREATE OR REPLACE FUNCTION public.get_stop_times_from_stop(target uuid, target_s
                 FROM
                     stop_times st2
                 WHERE
-                    st2.trip_id = stop_times.trip_id
-                    AND st2.stop_sequence > stop_times.stop_sequence)
+					st2.data_origin = stop_times.data_origin
+                    AND st2.trip_id = stop_times.trip_id
+                    AND st2.stop_sequence > stop_times.stop_sequence
+	LIMIT 1)
         ORDER BY
             calendar_dates.date::date,
             arrival_time ASC
         LIMIT 100;
+
+
 $BODY$;
 
 ALTER FUNCTION public.get_stop_times_from_stop(uuid, int, time WITHOUT time zone, date) OWNER TO dennis;
