daa21ce8eaba89ba428cf10db2ebef77af1e881e
Enes <9263360+EnessenE@users.noreply.github.com>
2024-07-07 18:47:10 +0200

    Create update_supplier_for_download.sql

---
+++ Procedures/update_supplier_for_download.sql
@@ -0,0 +1,17 @@
+-- PROCEDURE: public.merge_stop(text, text)
+-- DROP PROCEDURE IF EXISTS public.merge_stop(text, text);
+CREATE OR REPLACE PROCEDURE public.update_supplier_for_download(IN target text, IN last_update timestamp with time zone, IN pending boolean)
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+    UPDATE SupplierConfigurations
+    SET 
+        last_updated = last_update,
+        download_pending = marked
+    WHERE 
+        name = target;
+END;
+$BODY$;
+
+ALTER PROCEDURE public.update_supplier_for_download(text,  timestamp with time zone, boolean) OWNER TO dennis;
+
