1a58194f8cc799abcef63cf4e35a0f39505faca4
Enes <9263360+EnessenE@users.noreply.github.com>
2024-08-16 21:15:20 +0200

    Update update_supplier_for_download.sql

--- Procedures/update_supplier_for_download.sql
+++ Procedures/update_supplier_for_download.sql
@@ -1,6 +1,6 @@
 -- PROCEDURE: public.merge_stop(text, text)
 -- DROP PROCEDURE IF EXISTS public.merge_stop(text, text);
-CREATE OR REPLACE PROCEDURE public.update_supplier_for_download(IN target text, IN last_update timestamp with time zone, IN success boolean)
+CREATE OR REPLACE PROCEDURE public.update_supplier_for_download(IN target text, IN last_update timestamp with time zone, IN pending boolean)
 LANGUAGE 'plpgsql'
 AS $BODY$
 BEGIN
@@ -12,7 +12,7 @@ BEGIN
         lower(name) = lower(target);
 
 
-    if success = true then
+    if pending = true then
         UPDATE supplier_configurations
         SET 
             last_updated = last_update,
@@ -20,14 +20,14 @@ BEGIN
             latest_succesfull_import_id = supplier_configurations.import_id
         WHERE 
             lower(name) = lower(target);
-    elsif success = false then
+    elsif pending = false then
         UPDATE supplier_configurations
         SET 
-            last_updated = last_update,
+            last_attempt = last_update,
             download_pending = false
         WHERE 
             lower(name) = lower(target);
-    endif;
+    end if;
 END;
 $BODY$;
 
