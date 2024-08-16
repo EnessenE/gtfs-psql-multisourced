79cdba83964295fddb672a8b947f766ae2f53ed3
Enes <9263360+EnessenE@users.noreply.github.com>
2024-08-16 20:47:34 +0200

    Update get_shapes_from_trip.sql

--- Functions/get_shapes_from_trip.sql
+++ Functions/get_shapes_from_trip.sql
@@ -1,29 +1,32 @@
-DROP FUNCTION IF EXISTS public.get_shapes_from_trip(text);
 CREATE OR REPLACE FUNCTION public.get_shapes_from_trip(target uuid)
     RETURNS TABLE(
         id text,
         latitude double precision,
         longitude double precision,
-        SEQUENCE bigint
+        "sequence" bigint
 ,
             distance_travelled double precision,
             data_origin character varying(100))
         LANGUAGE 'sql'
         COST 100 VOLATILE PARALLEL UNSAFE ROWS 1000
         AS $BODY$
+    with trip_data AS (
+        SELECT * from trips 
+		WHERE trips.internal_id = target LIMIT 1
+    )
     SELECT
         shapes.id,
         latitude,
         longitude,
-        SEQUENCE,
+        "sequence",
         distance_travelled,
         shapes.data_origin
     FROM
         shapes
-        INNER JOIN trips ON trips.shape_id = shapes.id
     WHERE
-        trips.internal_id = target
+        shapes.id = (select shape_id from trip_data limit 1)
     ORDER BY
-        SEQUENCE DESC
+        "sequence" DESC
 $BODY$;
 
+select * from get_shapes_from_trip('be98ccb6-12c0-46a8-995c-4642d4069968')
