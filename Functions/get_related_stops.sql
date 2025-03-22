23ba3b8fa110e4191e35c4101feeb455ab660abb
Enes <9263360+EnessenE@users.noreply.github.com>
2025-03-22 12:32:57 +0100

    Don't relate stops to eachother with different types

--- Functions/get_related_stops.sql
+++ Functions/get_related_stops.sql
@@ -6,22 +6,24 @@ CREATE OR REPLACE FUNCTION public.get_related_stops(target uuid, target_stop_typ
     LANGUAGE 'sql'
     COST 100 VOLATILE PARALLEL UNSAFE ROWS 1000
     AS $BODY$
-    WITH stop_data AS(
-        SELECT 
-            related_stop
-        FROM
-            related_stops
-        WHERE
-            primary_stop = target limit 1)
-    SELECT distinct
+	WITH stop_data AS(
+		SELECT 
+			primary_stop, geo_location
+		FROM
+			related_stops	
+			INNER JOIN stops ON related_stops.related_stop = stops.internal_id and related_stops.related_data_origin = stops.data_origin
+		WHERE
+			primary_stop = target
+		limit 1)
+	SELECT
 		primary_stop id,
-        name,
-        stop_type
-    FROM
-        related_stops
-        INNER JOIN stops ON related_stops.related_stop = stops.internal_id and related_stops.related_data_origin = stops.data_origin
-    WHERE(primary_stop = target)
-    AND (stop_type != target_stop_type AND stop_type != 1000 )
+		name,
+		stop_type
+	FROM
+		stops
+	INNER JOIN related_stops ON related_stops.related_stop = stops.internal_id
+	WHERE
+	(ST_DWithin(stops.geo_location, (select geo_location from stop_data limit 1), 300, FALSE))
 $BODY$;
 
 ALTER FUNCTION public.get_related_stops(uuid, int) OWNER TO dennis;
