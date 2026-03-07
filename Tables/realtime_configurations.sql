fac70ecd905026520d5fe6440aafe22aed2d6a81
Enes <9263360+EnessenE@users.noreply.github.com>
2026-03-07 16:16:43 +0100

    Sync with realtime

--- Tables/realtime_configurations.sql
+++ Tables/realtime_configurations.sql
@@ -2,16 +2,16 @@
 
 -- DROP TABLE IF EXISTS public.realtime_configurations;
 
-CREATE TABLE public.realtime_configurations
+CREATE TABLE IF NOT EXISTS public.realtime_configurations
 (
-    supplier_configuration_name text NOT NULL,
+    supplier_configuration_name text COLLATE pg_catalog."default" NOT NULL,
     polling_rate interval NOT NULL,
-    last_attempt timestamp with time zone NULL,
+    last_attempt timestamp with time zone,
+    url text COLLATE pg_catalog."default" NOT NULL,
     enabled boolean DEFAULT true,
-    url text NOT NULL,
-    header_secret text NULL,
-    secret text NULL,
-    credits text NULL,
+    header text COLLATE pg_catalog."default",
+    header_secret text COLLATE pg_catalog."default",
+    credits text COLLATE pg_catalog."default",
     CONSTRAINT realtime_configuration_pkey PRIMARY KEY (supplier_configuration_name, url)
 )
 
@@ -19,3 +19,7 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.realtime_configurations
     OWNER to postgres;
+
+GRANT ALL ON TABLE public.realtime_configurations TO pgagent;
+
+GRANT ALL ON TABLE public.realtime_configurations TO postgres;
