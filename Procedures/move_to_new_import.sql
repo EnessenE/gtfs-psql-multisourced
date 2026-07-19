d8674a3945ef9c14f8001ea5adc3f47a7ad74d08
Enes <9263360+EnessenE@users.noreply.github.com>
2026-07-19 22:26:58 +0200

    Fix an issue where import would fail due to import_id behavior

--- Procedures/move_to_new_import.sql
+++ Procedures/move_to_new_import.sql
@@ -7,35 +7,36 @@ CREATE OR REPLACE PROCEDURE public.move_to_new_import(
 LANGUAGE 'plpgsql'
 AS $BODY$
 BEGIN
+    -- TODO: Fix in harvester + use parameter
     DELETE FROM calendar_dates
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
     DELETE FROM calendars
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
     DELETE FROM frequencies
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
     DELETE FROM pathway
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
     DELETE FROM routes
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
     DELETE FROM shapes
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
     DELETE FROM stop_times
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
     DELETE FROM stops
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
     DELETE FROM transfers
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
     DELETE FROM trips
-    WHERE (import_id != target_id or import_id is null) AND data_origin = data_origin_target;
+    WHERE (import_id != (select latest_succesfull_import_id from supplier_configurations where lower(name) = lower(data_origin_target) limit 1) or import_id is null) AND data_origin = data_origin_target;
 
 END;
 $BODY$;
